bfc0f1b67b11bb60ce6299c7e09d1e1d6d3ff347
TweakPY <69265406+Filza2@users.noreply.github.com>
2022-05-12 22:43:55 -0400

    Add files via upload

--- PT-TXT.py
+++ PT-TXT.py
@@ -1,8 +1,5 @@
-try:import requests,re,os
-except:
-	os.system('pip install requests')
-	os.system('pip install re')
-	print('[-] Done')
+try:import requests,re
+except Exception as FL:exit(FL)
 print("""
 ██████╗ ████████╗████████╗██╗  ██╗████████╗
 ██╔══██╗╚══██╔══╝╚══██╔══╝╚██╗██╔╝╚══██╔══╝
@@ -10,34 +7,23 @@ print("""
 ██╔═══╝    ██║╚════╝██║    ██╔██╗    ██║   
 ██║        ██║      ██║   ██╔╝ ██╗   ██║   
 ╚═╝        ╚═╝      ╚═╝   ╚═╝  ╚═╝   ╚═╝                                             
-<\> @TweakPY """)
+        By @TweakPY - @vv1ck """)
 print('-'*30)
 text=input("[+] Type Your Text: ")
 print('-'*30)
-h={
-	'Host': 'leaked.wiki',
-	'Cookie': 'socials=yes',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-	'Accept-Language': 'ar,en-US;q=0.7,en;q=0.3',
-	'Accept-Encoding': 'gzip, deflate',
-	'Content-Type': 'application/x-www-form-urlencoded',
-	'Content-Length': '16',
-	'Origin': 'https://leaked.wiki',
-	'Referer': 'https://leaked.wiki/paste',
-	'Upgrade-Insecure-Requests': '1',
-	'Sec-Fetch-Dest': 'document',
-	'Sec-Fetch-Mode': 'navigate',
-	'Sec-Fetch-Site': 'same-origin',
-	'Sec-Fetch-User': '?1',
-	'Te': 'trailers',
-	'Connection': 'close'}
-d=f'pdata={text}'
 try:
-	req=requests.post('https://leaked.wiki/paste',headers=h,data=d)
+	req=requests.post('https://leaked.wiki/paste',headers={'Host': 'leaked.wiki','Cookie': 'socials=yes','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded','Content-Length': '16','Origin': 'https://leaked.wiki','Referer': 'https://leaked.wiki/paste','Upgrade-Insecure-Requests': '1','Sec-Fetch-Dest': 'document','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Site': 'same-origin','Sec-Fetch-User': '?1','Te': 'trailers','Connection': 'close'},data=f'pdata={text}')
 	id=re.findall('<script type="text/javascript"> location.href = "./p/(.*?)"; </script>',req.text)[0]
 	local_paste=f'https://leaked.wiki/r/{id}.txt'
-	print(f'[+] Your Paste in a raw is: {local_paste}')
-	print('')
+	print(f'[+] Your Paste in a raw is: {local_paste}');print('')
 	print(f'[+] Your Text is: {requests.get(local_paste).text}')
-except:print('[~] Error !!')
+except:
+	try:
+		problem=re.findall('''        <div class="container">
+            <span class="status-reason">
+                <i class="fas fa-user-times fa-2x"></i> (.*?)
+            </span>
+        </div>''',req.text)[0]
+		if problem=='Account Suspended':print('[!] Sorry the website we use has a problem > "leaked.wiki"')
+		else:print('[!] Error !!')
+	except:print('[!] Error !!')
